d1490007c9fb476106c4c47a502003014a0275b6
sarim nadeem <115877996+samcodeonline@users.noreply.github.com>
2024-03-20 17:25:57 +0500

    change

--- fileManagement/task1(day2)/18-03-2024(ownPractise).cpp
+++ fileManagement/task1(day2)/18-03-2024(ownPractise).cpp
@@ -1,87 +1,155 @@
 // main.cpp
-#include <fstream>
 #include <iostream>
-#include <string>
-#include <vector>
+#include <fstream>
+#include <algorithm>
 
 using namespace std;
-struct User
-{
-    string first_name;
-    string user_name;
-    string password;
-};
-
-void display_users(vector<User> const& users)
-{
-    int n{};
-    for (auto const& u : users)
-    {
-        cout
-            << ++n << ". " << u.first_name
-            << "\n  user name : " << u.user_name
-            << "\n  password  : " << u.password
-            << "\n\n";
-    }
-    cout << "\n\n";
-}
-
 int main()
 {
-    string file_name{ "tableData.txt" };
+    string file_name = "tableData.txt";
 
-    // Open file for input.
-    ifstream ist{ file_name };
-    if (!ist.is_open())
+    /*  If you write a record number for each user (along with his 
+     other data), then you must read the entire file to discover 
+     the record number of the last record.
+      
+      That is what this first loop does.
+	*/
+    // Open the file for input.
+    ifstream userReadFile{ file_name };
+    if (!userReadFile.is_open())
     {
+    	// The cerr object in C++ is used to print error messages.
         cerr << "Could not open input file: " << file_name << "\n\n";
         return 1;
     }
 
-    // Read file into vector.
-    User u;
-    vector<User> users;
-    while (ist >> u.first_name >> u.user_name >> u.password)
-        users.push_back(u);
-    ist.close();
+    /*  Read all the records. Only the data from the final record 
+      is saved.
+     
+     This is the proper way to test for input failure.
+     The `eof` test used in your program is not recommended.
+     See the comments posted by other users.
+     
+     If any one of `a`, `b`, `c`, or `d` cannot be read,
+     then `userReadFile` will be placed into a failed state, 
+     and `userReadFile.fail()` will be `true`.
+     
+     This is a shortcut way to test `userReadFile.fail()`.
+     When used as the condition for an if-statement or 
+     while-loop, the expression `userReadFile >> x` will 
+     attempt to input `x`, and then return `!userReadFile.fail()`
+      to the if-statement or while-loop. Note the 
+     exclamation point operator `!`. This is the same 
+     as "while not fail" or "if not fail."
+     
+    */
 
-    cout << "AFTER READING FILE\n\n";
-    display_users(users);
+    int lastRecordNumber = 0;  // anticipating an empty file
+    int a;
+    string b, c, d;
+    while (userReadFile >> a >> b >> c >> d) {
+        lastRecordNumber = a;
+    }
+    userReadFile.close();
 
-    // Add a user.
-    cout 
-        << "ADD A USER\n\n"
-        << "Enter first name: ";
-    cin >> u.first_name;
-
-    cout << "Enter user name: ";
-    cin >> u.user_name;
-
-    cout << "Enter password: ";
-    cin >> u.password;
-    cout << "\n\n\n";
-
-    users.push_back(u);
-
-    cout << "AFTER ADDING A USER\n\n";
-    display_users(users);
-
+    /*  At this point, `lastRecordNumber` holds the number
+      of the last record that was read successfully.
+      This is the answer to your original question.
+      
+     Next, run a loop to input records from the user.
+      It is convenient to open the file for output only 
+     once, and hold it open during this loop. We will 
+     close the file when this loop is finished.
+	*/
     // Open file for output.
-    ofstream ost{ file_name };
-    if (!ost.is_open())
+    ofstream userWriteFile(file_name, ios::app);
+    if (!userWriteFile.is_open())
     {
         cerr << "Could not open output file: " << file_name << "\n\n";
         return 2;
     }
 
-    // Write file.
-    for (auto const& u : users)
-        ost << u.first_name << " " << u.user_name << " " << u.password << '\n';
-    ost.close();
+    string firstName, userName, password;
+    bool inputMore = true;
+    while (inputMore)
+    {
+        // Input record from user.
+        cout << "Input Data" << endl;
+        cout << "Name: ";
+        cin >> firstName;
+        cout << "User-Name: ";
+        cin >> userName;
+        cout << "Password: ";
+        cin >> password;
+        cout << endl;
+
+        /*  Write record to file.
+        // Note that `lastRecordNumber` is incremented before 
+        // writing it to the file.
+        //
+        // Output exactly one line, followed a by newline.
+        // Do not output any other newlines.
+        // Note that the tab character has fallen out of favor.
+        // You can use `setw` to get better control of 
+        // alignment. For now, however, the tab remains.
+		*/
+        userWriteFile
+            << ++lastRecordNumber
+            << ' ' << firstName
+            << '\t' << userName
+            << '\t' << password
+            << '\n';
+
+        /*  Ask user whether to input more records.
+          Keep the Yes/No code together. Run a small loop
+          to trap invalid entries.
+        */ 
+//        for (;;)
+		while(inputMore == true)
+        {
+            cout << "Do You Want more Entries or Not (Press the Key Y(Yes) / N(No)): ";
+            char character;
+            cin >> character;
+            cout << '\n';
+            if (character == 'y' || character == 'Y') {
+                inputMore = true;
+                break;  // break out of the for-loop
+            }
+            else if (character == 'n' || character == 'N') {
+                inputMore = false;
+                break;  // break out of the for-loop
+            }
+            else {
+                cout << "Invalid Character\n\n";
+            }
+        }
+    }
+
+    // Now we close the output file.
+    userWriteFile.close();
+
+    // All that remains is to read and display the file.
+    // Note that this loop should not be placed inside 
+    // the loop the writes to the file.
+
+    // Open the file for input.
+    userReadFile.open(file_name);
+    if (!userReadFile.is_open())
+    {
+        cerr << "Could not open input file: " << file_name << "\n\n";
+        return 3;
+    }
+
+    while (userReadFile >> a >> b >> c >> d) {
+        cout << a << ". " << b << "  " << c << "  " << d << "\n\n";
+    }
+    userReadFile.close();
     return 0;
+    
+    
+    
 }
 // end file: main.cpp
-
 
 
 
--- fileManagement/task1(day2)/Write,Read,Edit,Delete.cpp
+++ fileManagement/task1(day2)/Write,Read,Edit,Delete.cpp
@@ -1,61 +1,130 @@
+// main.cpp
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+
 using namespace std;
-//using namespace cout;
-int main(){
+int main()
+{
+    string file_name = "tableData.txt";
+    ifstream userReadFile{ file_name };
+    if (!userReadFile.is_open())
+    {
+    	// The cerr object in C++ is used to print error messages.
+        cerr << "Could not open input file: " << file_name << "\n\n";
+        return 1;
+    }
+
+    int lastRecordNumber = 0;  // anticipating an empty file
+    int a;
+    string b, c, d;
+    while (userReadFile >> a >> b >> c >> d) {
+        lastRecordNumber = a;
+    }
+    userReadFile.close();
+
+    // Open file for output.
+    ofstream userWriteFile(file_name, ios::app);
+    if (!userWriteFile.is_open())
+    {
+        cerr << "Could not open output file: " << file_name << "\n\n";
+        return 2;
+    }
+
+    string firstName, userName, password;
+    bool inputMore = true;
+    while (inputMore)
+    {
+        // Input record from user.
+        cout << "Input Data" << endl;
+        cout << "Name: ";
+        cin >> firstName;
+        cout << "User-Name: ";
+        cin >> userName;
+        cout << "Password: ";
+        cin >> password;
+        cout << endl;
+
+        userWriteFile
+            << ++lastRecordNumber
+            << ' ' << firstName
+            << '\t' << userName
+            << '\t' << password
+            << '\n';
+
+//        for (;;)
+		while(inputMore)
+        {
+            cout << "Do You Want more Entries or Not (Press the Key Y(Yes) / N(No)): ";
+            char character;
+            cin >> character;
+            cout << '\n';
+            if (character == 'y' || character == 'Y') {
+                inputMore = true;
+                break;  // break out of the for-loop
+            }
+            else if (character == 'n' || character == 'N') {
+                inputMore = false;
+                break;  // break out of the for-loop
+            }
+            else {
+                cout << "Invalid Character\n\n";
+                break;
+            }
+        }
+    }
+
+    // Now we close the output file.
+    userWriteFile.close();
+
+    // Open the file for input.
+    userReadFile.open(file_name);
+    if (!userReadFile.is_open())
+    {
+        cerr << "Could not open input file: " << file_name << "\n\n";
+        return 3;
+    }
+
+    while (userReadFile >> a >> b >> c >> d) {
+        cout << a << ". " << b << "  " << c << "  " << d << "\n\n";
+    }
+    userReadFile.close();
+    
+    
+    // open file to read the content for Updating record
+    userReadFile.open(file_name);
+    int input;
+    cout << "Input the id that you wish to find" << endl;
+	cin >> input;
+
 	cout << "\n";
-	int i = 1; 
-	
-	while(i > 0){
-		cout << "Input Data" << endl;
-		string   firstName , userName , password;
-		char character;
-		ofstream userWriteFile("tableData.txt" , ios::app);
-		cout << "Name: ";
-		cin >> firstName;
-		cout << "User-Name: ";
-		cin >> userName;
-		cout << "Password: ";
-		cin >> password;
-		
-		userWriteFile << "\n";
-		int n = 0;
-		n =  i + 0;
-		userWriteFile << n << " " ;
-		userWriteFile << firstName << "\t\t";
-		userWriteFile << userName << "\t\t";
-		userWriteFile << password << "\t\t";
-		
-		userWriteFile.close();
-		
-		cout << "Do You Want more Entries or Not (Press the Key Y(Yes) / N(No)): ";
-		cin >> character;
-		if(character == 'y' || character == 'Y'){
-			i++;
-		}
-		else if(character == 'n' || character == 'N'){
-		//otherWise print the table 
-		ifstream userReadFile("tableData.txt");
-				               
-		        while ( 1 ){
-	            userReadFile >> n >> firstName >> userName >> password;     
-		        if(userReadFile.eof())  
-		            break;              
-					cout << "\n";     
-		            cout << n << "." << " " << firstName << "  " << userName << "  " << password << "  " << "\n";
-		        }
-		        
-				break;
-				userReadFile.close();
-		}
-		else{
-			cout << "invalid Character";
-			break;
-		}
+	while(userReadFile >> a >> b >> c >> d)
+	{
+	   if (input == a) {
+	     cout << a << ". " << b << "  " << c << "  " << d << "\n\n";
+	    }
 	}
-	return 0;
+	
+	cout << "\n";
+	cout << "The user to update is as follow" << endl;
+    userReadFile.close();
+	// open file to write the updated record
+	
+//	std::fstream s(my_file_path); // use option std::ios_base::binary if necessary
+	
+	userWriteFile.open(file_name, ios_base::binary);
+	userWriteFile.seekp(, ios_base::beg);
+	userWriteFile.write(my_data, size_of_data_to_overwrite);
+    
+    userWriteFile.close();
+    
+    return 0;
 }
+// end file: main.cpp
+
+
+
+
 
 
 
